4f772e9cc0dd10a864bb965dfc075ccc150b52bd
vinaypoduri888 <128017007+vinaypoduri888@users.noreply.github.com>
2023-04-04 13:28:49 +0530

    Update udemy.py

--- udemy.py
+++ udemy.py
@@ -103,6 +103,7 @@ if show_q13:
     sns.histplot(x='price', data=data, bins=20, ax=ax)
     ax.set_xlabel("Price")
     ax.set_ylabel("Number of courses")
+    plt.xticks(rotation = 'vertical')
     st.pyplot(fig)
 show_q14 = st.checkbox("Number of paid and free courses")
 if show_q14:
